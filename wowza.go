f7c2f88f704d4f24475e96aecfe1c26418a37280
Francisco Fernandez <francisco.fernandez.castano@gmail.com>
2014-12-22 16:35:48 +0100

    Adding delete capabilities.

--- wowza.go
+++ wowza.go
@@ -37,6 +37,13 @@ func createDirs(streamId string) {
 	check(err)
 }
 
+func removeDirs(streamId string) {
+	err := os.RemoveAll(filepath.Join(WOWZA_HOME_APPS, streamId))
+	check(err)
+	err = os.RemoveAll(filepath.Join(WOWZA_HOME_CONF, streamId))
+	check(err)
+}
+
 func createConfFiles(streamId, port string) string {
 	streamFile := streamId + ".stream"
 	confDir := filepath.Join(WOWZA_HOME_CONF, streamId)
@@ -52,20 +59,30 @@ func createConfFiles(streamId, port string) string {
 	return streamFile
 }
 
-func createWowzaApp(streamId, streamFile string) {
-	startReq := url.Values{"action": {"startStream"},
+func createWowzaStream(streamId, streamFile string) {
+	startVals := url.Values{"action": {"startStream"},
 		"appName": {streamId + "/_definst_"}, "streamName": {streamFile},
 		"mediaCasterType": {"rtp"}, "vhostName": {"undefined"}}
+	authRequestWowza(startVals)
+}
 
+func removeWowzaStream(streamId, streamFile string) {
+	deleteVals := url.Values{"action": {"deleteStream"},
+		"appName": {streamId + "/_definst_"}, "streamName": {streamFile},
+		"mediaCasterType": {"rtp"}, "vhostName": {"undefined"}}
+	authRequestWowza(deleteVals)
+}
+
+func authRequestWowza(reqBody url.Values) {
 	client := &http.Client{}
 
-	req, _ := http.NewRequest("POST", WOWZA_STREAM_API, strings.NewReader(startReq.Encode()))
+	req, _ := http.NewRequest("POST", WOWZA_STREAM_API, strings.NewReader(reqBody.Encode()))
 	username := "rushmore"
 	password := "rushmore"
 	resp, _ := client.Do(req)
 
 	if resp.StatusCode == 401 {
-		reqx, _ := http.NewRequest("POST", WOWZA_STREAM_API, strings.NewReader(startReq.Encode()))
+		reqx, _ := http.NewRequest("POST", WOWZA_STREAM_API, strings.NewReader(reqBody.Encode()))
 		httpdigest.SetDigestAuth(reqx, username, password, resp, 1)
 		_, erx := client.Do(reqx)
 		check(erx)
@@ -82,10 +99,15 @@ func calculatePort(archiveId string) int64 {
 	return (10000 + hexId) % 30011
 }
 
-func generateWowzaStream(streamId, port string) {
+func generateWowzaApp(streamId, port string) {
 	createDirs(streamId)
 	streamFile := createConfFiles(streamId, port)
-	createWowzaApp(streamId, streamFile)
+	createWowzaStream(streamId, streamFile)
+}
+
+func deleteWowzaApp(streamId, port string) {
+	removeWowzaStream(streamId, port)
+	removeDirs(streamId)
 }
 
 func main() {
@@ -96,10 +118,13 @@ func main() {
 		decoder.Decode(&broadcast)
 		port := calculatePort(broadcast.Id)
 		streamId := broadcast.Id
-		generateWowzaStream(streamId, strconv.FormatInt(port, 10))
+		generateWowzaApp(streamId, strconv.FormatInt(port, 10))
 	})
-	m.Delete("/streams/:archiveid", func(params martini.Params) string {
-		return params["archiveid"]
+	m.Delete("/streams/:archiveid", func(params martini.Params) {
+		port := calculatePort(params["archiveid"])
+		streamId := params["archiveid"]
+		deleteWowzaApp(streamId, strconv.FormatInt(port, 10))
+
 	})
 
 	m.Run()
